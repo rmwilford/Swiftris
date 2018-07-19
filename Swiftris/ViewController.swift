bfedecc0eb9399bd39f3587fef139b7501082695
Denise <student@rwilford.local>
2018-07-19 13:37:11 -0500

    Added sound files, launch image, app icons, set up the normal view controller.

--- Swiftris/ViewController.swift
+++ Swiftris/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
+// this view controller not used.
+// entry view controller is SwiftrisViewController.
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override var prefersStatusBarHidden : Bool {
+        return false
+    }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+        let swiftrisViewController = SwiftrisViewController()
+        self.present(swiftrisViewController, animated: true, completion: nil)
+    }
 }
 
